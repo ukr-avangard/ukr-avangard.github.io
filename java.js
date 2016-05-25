1765960cbcf95922e19ec8796fcd1463aa94df8d
sasha-popov <tansked2000@gmail.com>
2016-05-25 08:43:26 +0300

    visota

--- java.js
+++ java.js
@@ -8,7 +8,7 @@ function show1(argument1, input, block)
 		{
 			div.style.display = 'block';
 			input.value="Закрити калькулятор";
-            conteiner.style.height="580px";			
+            conteiner.style.height="585x";			
 		}
 		else
 		{
